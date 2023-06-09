aaaab5380d614f7102f1d6714f669077f5f5ea45
chirag-purohit <chirag@gmail.com>
2023-06-09 18:54:39 +0530

    this the second commit

--- main.tf
+++ main.tf
@@ -138,30 +138,6 @@ resource "aws_route_table_association" "route-asso-3" {
   route_table_id = aws_route_table.private_rt2.id
 }
 
-
-## making instance in this vpc
-resource "aws_instance" "server" {
-    ami = "ami-063e1495af50e6fd5"
-    instance_type = "t2.micro"
-    key_name = "singapore_keys"
-    availability_zone = "ap-southeast-1a"
-    subnet_id = aws_subnet.public-subnet.id
-    vpc_security_group_ids = [aws_security_group.project-sg.id]
-
-    tags = { 
-        "Name" = "server"
-    }
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo yum update -y
-              sudo yum install -y docker
-              sudo systemctl start docker
-              sudo systemctl enable docker
-              sudo docker run -dit --name wordpress -p 80:80 wordpress
-              sudo yum install -y mysql
-              EOF
-}
-
 resource "aws_eip" "eip" {
   instance = aws_instance.server.id
   vpc      = true
